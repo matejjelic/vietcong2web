9b096d9ec5d3924b900d8734dcfc6cf9f489b596
Matej Jelić <matej.jelic92@gmail.com>
2016-06-03 22:04:08 +0200

    Adding favicon

--- layout/header.php
+++ layout/header.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title><?php if(isset($title)){ echo $title; }?></title>
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="style/main.css">
-</head>
-<body>
+    <head>
+        <meta charset="utf-8">
+        <title><?php if (isset($title)) {
+    echo $title;
+} ?></title>
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="style/main.css">
+        <link rel="shortcut icon" href="favicon.ico" />
+    </head>
+    <body>
--- layout/headerFileBrowser.php
+++ layout/headerFileBrowser.php
@@ -2,13 +2,16 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title><?php if (isset($title)) {
-    echo $title;
-} ?></title>
+        <title><?php
+            if (isset($title)) {
+                echo $title;
+            }
+            ?></title>
         <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="style/main.css">
 
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <link href="assets/css/styles.css" rel="stylesheet"/>
+        <link rel="shortcut icon" href="favicon.ico" />
     </head>
     <body>
